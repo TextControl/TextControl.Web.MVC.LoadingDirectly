df597360224ee5270bc90ed0869b26b18feed2f4
Bjoern Meyer <bjoern@textcontrol.com>
2019-07-22 16:28:32 +0200

    Initial upload

---
+++ tx_wshandler/Controllers/TXDocumentController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TXTextControl.Web;
+
+namespace tx_wshandler.Controllers
+{
+    public class TXDocumentController : ApiController
+    {
+        [System.Web.Http.HttpGet]
+        public HttpResponseMessage LoadDocument(string ConnectionID, string DocumentName)
+        {
+            // connect the WebSocketHandler with the ConnectionID
+            WebSocketHandler wsHandler = WebSocketHandler.GetInstance(ConnectionID);
+
+            // the document directly server-side
+            wsHandler.LoadText(System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data/" + DocumentName),
+                StreamType.WordprocessingML);
+
+            return new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK
+            };
+        }
+
+        [System.Web.Http.HttpGet]
+        public HttpResponseMessage SaveDocument(string ConnectionID, string DocumentName)
+        {
+            // connect the WebSocketHandler with the ConnectionID
+            WebSocketHandler wsHandler = WebSocketHandler.GetInstance(ConnectionID);
+
+            // save the document directly server-side
+            wsHandler.SaveText(System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data/" + DocumentName),
+                StreamType.WordprocessingML);
+
+            return new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK
+            };
+        }
+    }
+}
